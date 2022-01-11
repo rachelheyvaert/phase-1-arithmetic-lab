95bde9ad762164a653d9b3795e51d03377722c25
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-11 07:26:08 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//test 1
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+//test 2
+let random = Math.floor(Math.random() * 10) + 1;
+
+//test 3
+let num3 = 16;
+let num4 = 4;
+let mod =  num3 / num4;
+
+//test 4
+
+let max = Math.max(5, 10, 15, 20);
